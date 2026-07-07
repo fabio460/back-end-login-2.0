bd5cb437fb935c77416d9ab5b7894de9029b5ed6
Fabio <dasilvadeoliveirafabio@email.com>
2026-07-06 23:02:23 -0300

    algumas rotas criadas

--- controllers/usuarioController.ts
+++ controllers/usuarioController.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const chaveSecreta = "chaveSecreta"
 const prisma = new PrismaClient();
 
 export const getUsuario = async(req:Request, res:Response) =>{
@@ -11,3 +14,70 @@ export const getUsuario = async(req:Request, res:Response) =>{
         res.json("erro")
     }
 }
+
+export const criarUsuario = async(req: Request, res: Response)=>{
+    const {email, nome, senha} = req.body
+    if (!email || !nome || !senha) {
+        res.json("dados em branco")
+        return null
+    }
+    try {
+        const existeUsuario = await prisma.usuario.findUnique({
+            where:{email}
+        })
+        if (existeUsuario) {
+            res.json("Já existe este usuário!")
+        }
+        const salt =await bcrypt.genSalt(10)
+        const senhaHasheada =await bcrypt.hash(senha,salt)
+        const usuario = await prisma.usuario.create({
+            data:{
+                email,
+                nome,
+                senha:senhaHasheada
+            }
+        })
+        res.status(200).json("usuario criado com sucesso!")
+    } catch (error) {
+        res.status(400).json("Falha ao criar usuario!")       
+    }
+}
+
+export const logarUsuario = async(req:Request, res:Response)=>{
+    const {email, senha} = req.body
+    if (!email || !senha) {
+        res.status(401).json("Dados")
+    }
+    try {
+        const usuario = await prisma.usuario.findUnique({
+            where:{
+                email
+            }
+        })
+        if (senha !== usuario?.senha) {
+            res.json("Usuário ou senha inválidos!")
+        }
+        
+        const token = jwt.sign({usuario},chaveSecreta,{expiresIn:"1d"})
+        res.status(200).json(token)
+        
+    } catch (error) {
+        res.status(401).json("Falha ao fazer login!")
+    }
+}
+
+export const autenticarUsuario = async (req:Request, res:Response)=>{
+    try {
+        const tokenString = req.headers.authorization?.split(' ')[1];
+        const token =JSON.parse(tokenString as string);
+        if (!token) {
+            res.status(401).json("token inválido")
+        }
+        const tokenValido = jwt.verify(token,chaveSecreta) as string
+        res.status(200).json(jwt.decode(tokenValido))
+        
+    } catch (error) {
+        res.status(401).json("falha na autenticação")
+    }
+}
+
--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
-import { getUsuario } from "./controllers/usuarioController.js";
+import { autenticarUsuario, criarUsuario, getUsuario, logarUsuario } from "./controllers/usuarioController.js";
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.get("/usuario",getUsuario)
+app.post("/criarusuario",criarUsuario)
+app.post("/logarusuario",logarUsuario)
+app.post("/autenticarusuario",autenticarUsuario)
 app.listen(4000,()=>console.log("rodando na porta 4000"))
